5f83288e347c5cbc6ccb514e7ad666f23b2f375c
Opeyemi Alabi <91141140+horluwaluvme@users.noreply.github.com>
2022-10-21 22:32:03 +0100

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,13 +3,13 @@
 given URL & email as params, display response body utf-8, print error codes
 usage: ./3-error_code.py http://0.0.0.0:5000/status_401
 """
-from sys import argv
 import urllib.request
-
+from urllib.error import HTTPError
+import sys
 
 if __name__ == "__main__":
     try:
-        with urllib.request.urlopen(argv[1]) as response:
-            print(response.read().decode('utf-8'))
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8', "replace"))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
