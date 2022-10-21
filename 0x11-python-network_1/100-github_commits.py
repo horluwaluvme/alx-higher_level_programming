0cb29434702e5d176203350a5086500fd26d61a8
Opeyemi Alabi <91141140+horluwaluvme@users.noreply.github.com>
2022-10-21 22:42:59 +0100

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,6 +3,7 @@
 given repo and owner name as argvs, use Github API to list last 10 commits
 usage: ./100-github_commits.py [github_repo] [github_owner]
 """
+"""
 from sys import argv
 import requests
 
@@ -14,4 +15,17 @@ if __name__ == "__main__":
     list_commits = r.json()
     for commit in list_commits[0:10]:
         print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
+        print(commit.get('commit').get('author').get('name'))"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(sys.argv[2], sys.argv[1]))
+    if r.status_code >= 400:
+        print('None')
+    else:
+        for com in r.json()[:10]:
+            print("{}: {}".format(com.get('sha'),
+                                  com.get('commit').get('author').get('name')))
