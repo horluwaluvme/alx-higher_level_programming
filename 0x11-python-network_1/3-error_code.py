6205d79de91c058700bea1d37202de58668079ce
Opeyemi Alabi <91141140+horluwaluvme@users.noreply.github.com>
2022-10-21 22:33:17 +0100

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     try:
         with urllib.request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8', "replace"))
-    except urllib.error.HTTPError as e:
+    except HTTPError as e:
         print("Error code: {}".format(e.code))
